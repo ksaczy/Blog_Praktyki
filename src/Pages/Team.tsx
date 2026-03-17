c6253ca6ab6f9720d968ca1c38267d858c683542
ksaczy <ksaczy@proton.me>
2026-03-17 10:06:40 +0100

    implement infinite loop carousel for Team section :
    Added Swiper.js integration
    
    Implemented 3D coverflow effect with blur for side slides
    
    Updated Team.tsx structure and SCSS styling for responsiveness

--- src/Pages/Team.tsx
+++ src/Pages/Team.tsx
@@ -1,3 +1,9 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, EffectCoverflow, Pagination } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
 import './Team.scss';
 
 interface TeamMember {
@@ -7,60 +13,50 @@ interface TeamMember {
     description: string;
 }
 
-
 const Team = () => {
-
     const members: TeamMember[] = [
-        {
-            name: "Emilian",
-            surname: "Hernes",
-            img: "ProfilePicture1.jpg",
-            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad assumenda consectetur et quasi quis quos rem similique sit soluta vitae!"
-        },
-        {
-            name: "Emilian",
-            surname: "Hernes",
-            img: "ProfilePicture2.jpg",
-            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad assumenda consectetur et quasi quis quos rem similique sit soluta vitae!"
-        },
-        {
-            name: "Emilian",
-            surname: "Hernes",
-            img: "ProfilePicture3.jpg",
-            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad assumenda consectetur et quasi quis quos rem similique sit soluta vitae!"
-        },
-        {
-            name: "Emilian",
-            surname: "Hernes",
-            img: "ProfilePicture4.jpg",
-            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad assumenda consectetur et quasi quis quos rem similique sit soluta vitae!"
-        },
-        {
-            name: "Emilian",
-            surname: "Hernes",
-            img: "ProfilePicture5.jpg",
-            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad assumenda consectetur et quasi quis quos rem similique sit soluta vitae!"
-        },
-        {
-            name: "Emilian",
-            surname: "Hernes",
-            img: "ProfilePicture6.jpg",
-            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad assumenda consectetur et quasi quis quos rem similique sit soluta vitae!"
-        }
-    ]
+        { name: "Emilian", surname: "Hernes", img: "ProfilePicture1.jpg", description: "Lorem ipsum..." },
+        { name: "Emilian", surname: "Hernes", img: "ProfilePicture2.jpg", description: "Lorem ipsum..." },
+        { name: "Emilian", surname: "Hernes", img: "ProfilePicture3.jpg", description: "Lorem ipsum..." },
+        { name: "Emilian", surname: "Hernes", img: "ProfilePicture4.jpg", description: "Lorem ipsum..." },
+        { name: "Emilian", surname: "Hernes", img: "ProfilePicture5.jpg", description: "Lorem ipsum..." },
+        { name: "Emilian", surname: "Hernes", img: "ProfilePicture6.jpg", description: "Lorem ipsum..." }
+    ];
 
     return (
-        <div className="team">
-            {members.map((member, i)=> (
-                <section key={i} className="pearson">
-                    <img src={`/Images/${member.img}`} alt={member.name}/>
-                    <h2>{member.name} {member.surname}</h2>
-                    <p>{member.description}</p>
-                </section>
-            ))}
-
+        <div className="team-container">
+            <Swiper
+                effect={'coverflow'}
+                grabCursor={true}
+                centeredSlides={true}
+                slidesPerView={'auto'}
+                loop={true}
+                coverflowEffect={{
+                    rotate: 0,
+                    stretch: 0,
+                    depth: 100,
+                    modifier: 2.5,
+                    slideShadows: false,
+                }}
+                autoplay={{
+                    delay: 3000,
+                    disableOnInteraction: false,
+                }}
+                modules={[EffectCoverflow, Pagination, Autoplay]}
+                className="mySwiper"
+            >
+                {members.map((member, i) => (
+                    <SwiperSlide key={i}>
+                        <section className="person-card">
+                            <img src={`Images/${member.img}`} alt={member.name} />
+                            <h2>{member.name} {member.surname}</h2>
+                            <p>{member.description}</p>
+                        </section>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
         </div>
-    )
+    );
 }
 
 export default Team;
